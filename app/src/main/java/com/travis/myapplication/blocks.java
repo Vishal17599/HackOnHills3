25fc1def09d96b6dc02839bf24e553059ebcf279
Vishal17599 <Vishal.parmar17599@gmail.com>
2018-03-25 11:52:22 +0530

    id added

--- app/src/main/java/com/travis/myapplication/blocks.java
+++ app/src/main/java/com/travis/myapplication/blocks.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Chobarla on 3/25/2018.
@@ -19,6 +20,33 @@ public class blocks extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(blocks.this,floors.class) );
+            }
+        });
+        findViewById(R.id.block_b).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(blocks.this, "Will be added soon!", Toast.LENGTH_SHORT).show();;
+            }
+        });
+
+        findViewById(R.id.block_c).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(blocks.this, "Will be added soon!", Toast.LENGTH_SHORT).show();;
+            }
+        });
+
+        findViewById(R.id.block_d).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(blocks.this, "Will be added soon!", Toast.LENGTH_SHORT).show();;
+            }
+        });
+
+        findViewById(R.id.block_e).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(blocks.this, "Will be added soon!", Toast.LENGTH_SHORT).show();;
             }
         });
     }
--- app/src/main/java/com/travis/myapplication/hostels_name.java
+++ app/src/main/java/com/travis/myapplication/hostels_name.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class hostels_name extends AppCompatActivity {
 
@@ -19,6 +20,76 @@ public class hostels_name extends AppCompatActivity {
                 startActivity(new Intent(hostels_name.this,blocks.class));
             }
         });
+        findViewById(R.id.manimahesh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.himgiri).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.himadri).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.shivalik).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.vidyanchal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.satpura).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.ambika).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.parvati).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.dauladhar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.neelkanth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(hostels_name.this, "Will be added soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
 
